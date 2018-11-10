5e24b21aab18cf636f9dfa0226ad154d9b21dbd6
Holger Mueller <9jholger@gmail.com>
2018-11-10 13:49:52 -0600

    began calculator build

--- Test.java
+++ Test.java
@@ -1,8 +1,16 @@
-public class Test {
+// public class Test {
 
-    public static void main(String[] args) {
-        String x = "TEST";
-        System.out.println(x);
+//     public static void main(String[] args) {
+//         String x = "Hello world!";
+//         System.out.println(x);
+//     }
+
+// }
+
+import java.util.*;
+
+public class Sum {
+    public static void methodname() {
+        
     }
-
 }
